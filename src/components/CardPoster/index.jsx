1e62b33826fc6b06e7acf8b3dfe9256681be226b
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-19 19:28:28 -0300

    Criado componente CardPoster

---
+++ src/components/CardPoster/index.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { Icon } from '@iconify/react';
+import styles from "./CardPoster.module.css";
+
+export default function CardPoster({donghua}) {
+    return (
+        <div className={styles.cardDonghua}>
+            <p className={styles.donghuaName}>
+                {donghua.title}
+            </p>
+            <div className={styles.cardPoster}>
+                <Link to={`/donghua/${donghua.title}`} >
+                    <img 
+                        className={styles.posterImg} 
+                        src={donghua.poster}
+                        alt={`Imagem do Donghua ${donghua.title}`} 
+                    />
+                </Link>
+                <div className={styles.cardButtons}>
+                    <Link to={`/editar-donghua/${donghua.title}`}>
+                        <Icon 
+                            className={styles.svg} 
+                            icon="mdi:pencil-box" 
+                            width="24" 
+                            color="white" 
+                        />
+                    </Link>
+                    <Icon
+                        className={styles.svg}
+                        icon="ic:baseline-delete"
+                        width="24"
+                        color="white"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
