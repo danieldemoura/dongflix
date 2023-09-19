9fef2f93c6b4d9439ba7353940b3cf81d8d48111
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-19 14:02:15 -0300

    removido o texto git

--- src/components/CardDonghua/index.jsx
+++ src/components/CardDonghua/index.jsx
@@ -35,7 +35,7 @@ export function CardDonghua({donghuasList, currentDonghua}) {
 
     },[donghuasList, updateComponent])
     
-    git
+    
     return (
         <ul className={styles.relatedDonghuas}>
             {relatedDonghuas.length > 0 &&
