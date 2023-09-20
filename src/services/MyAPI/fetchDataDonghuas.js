067a0475f0738dbe51a51acf28ded7f9710c399a
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-20 18:01:11 -0300

    alterado o nome para updateDataDonghuas

--- src/services/MyAPI/fetchDataDonghuas.js
+++
@@ -1,6 +0,0 @@
-export async function fetchDataDonghuas(data) {
-    const response = await fetch(`http://localhost:3001/donghuas?status="em estreia"`);
-    const donghuas = await response.json();
-
-    return donghuas
-}
