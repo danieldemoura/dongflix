ee0135a7638e47a5c512dee9fbda31924586dd2f
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-20 18:11:57 -0300

    componente Index refatorado

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -4,38 +4,56 @@ import Header from "../../components/Header";
 import Headline from "../../components/Headline";
 import ButtonLink from "../../components/ButtonLink";
 import SlideShow from "../../components/SlideShow";
+import fetchData from "../../services/MyAPI/fetchData";
 import styles from "./Index.module.css";
+import { useEffect, useState } from "react";
 
 export default function Index() {
-    const [banner] = useApiData("advertisements");
-    const { title, message, slideShow } = banner.length > 0 ? banner.slice(0, 1)[0] : [];    
-    const advertisements = banner.slice(1);
+    const [slideShow, setSlideShow] = useState({});
+    const [advertisements, setAdvertisements] = useState([])
+
+    useEffect(() => {
+        fetchData("advertisements")
+        .then(json => {
+            const slideShow = json.slice(0, 1)[0];
+            setSlideShow(slideShow);
+
+            setAdvertisements(json.slice(1));
+        })
+        .catch(error => {
+            console.error("Erro ao buscar dados do banner:", error);
+        });
+        
+    }, []);
+        
 
     return (
         <>
             <Header>
-                <Headline title={title} text={message}>
+                <Headline title={slideShow.title} text={slideShow.message}>
                     <p className={styles.description}>
-                        { message }
+                        {slideShow.message}
                     </p>
                     <ButtonLink typeStyle="primary" url="sign-up">ASSINAR DONGFLIX</ButtonLink>
                     <ButtonLink url="login">JÁ SOU ASSINANTE</ButtonLink>
                 </Headline>
 
-                <SlideShow slideShow={slideShow}/>
+                <SlideShow slideShow={slideShow.slideShow}/>
             </Header>
 
-            {advertisements.map((section) => (
-                <section className={styles.sectionImage} style={{backgroundImage: `url(${section.image})`}} key={section.id}>
-                    <div className={styles.sectionContainer}>
-                        <h2 className={styles.sectionTitle}>
-                            {section.title}
-                        </h2>
-                        <p className={styles.description}>
-                            {section.message}
-                        </p>
-                    </div>
-                </section>)) 
+            {
+                advertisements.map((section) => (
+                    <section className={styles.sectionImage} style={{backgroundImage: `url(${section.image})`}} key={section.id}>
+                        <div className={styles.sectionContainer}>
+                            <h2 className={styles.sectionTitle}>
+                                {section.title}
+                            </h2>
+                            <p className={styles.description}>
+                                {section.message}
+                            </p>
+                        </div>
+                    </section>)
+                ) 
             }
 
             <SubscriptionPlans />
