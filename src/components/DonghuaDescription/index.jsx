6f6666d83dc958b45a34ba68a8cd25544fcbae74
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-21 18:43:13 -0300

    componente refatorado

--- src/components/DonghuaDescription/index.jsx
+++ src/components/DonghuaDescription/index.jsx
@@ -1,5 +1,5 @@
 import { Icon } from '@iconify/react';
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { FavoriteContext } from '../../contexts/FavoriteContext';
 import { useParams } from 'react-router-dom';
 import styles from "./DonghuaDescription.module.css";
@@ -9,7 +9,6 @@ export default function DonghuaDescription({ text, release, genere, age, status,
 
     const { toggleFavorite, favoriteList } = useContext(FavoriteContext);
     const currentDonghua = useParams();
-
     const isFavorite = favoriteList.includes(currentDonghua.name);
 
     function favorite() {
@@ -18,23 +17,23 @@ export default function DonghuaDescription({ text, release, genere, age, status,
 
     return (
         <div className={styles.container}>
-                <h1 className={styles.title}>{title}</h1>
-                <span className={styles.releaseAndGenere}>Estreia {release} | {genere}</span>
-                <span className={styles.wrapperStatus}>
-                    <span className={styles.status}>Status: {status}</span>
-                    <Icon className={styles.favorite} 
-                        icon={`${isFavorite ? "ic:outline-favorite" : "ic:outline-favorite-border"}`}
-                        width="100%" 
-                        height="100%"
-                        color={`${isFavorite ? "red" : "white"}`}
-                        onClick={favorite}
-                    />
-                </span>
+            <h1 className={styles.title}>{title}</h1>
+            <span className={styles.releaseAndGenere}>Estreia {release} | {genere}</span>
+            <span className={styles.wrapperStatus}>
+                <span className={styles.status}>Status: {status}</span>
+                <Icon className={styles.favorite} 
+                    icon={`${isFavorite ? "ic:outline-favorite" : "ic:outline-favorite-border"}`}
+                    width="100%" 
+                    height="100%"
+                    color={`${isFavorite ? "red" : "white"}`}
+                    onClick={favorite}
+                />
+            </span>
 
-                <span className={styles.releaseAndGenere}>Classificação: {age} | {amountOfSeason}</span>
-                <p className={styles.description}>
-                    {text}
-                </p>
-            </div>
+            <span className={styles.releaseAndGenere}>Classificação: {age} | {amountOfSeason}</span>
+            <p className={styles.description}>
+                {text}
+            </p>
+        </div>
     )
 }
